6e3754c1895606c704e75146c3394dc4d4dab3a6
eadwu <fangkazuto@gmail.com>
2018-01-20 20:55:36 -0500

    Update preprocessing output

--- bot/app/preprocessor.js
+++ bot/app/preprocessor.js
@@ -1,11 +1,10 @@
 const Log = require('../api/Log')
 
 function preProcessor (gameMap) {
-  Log.log(
-    `no data pre-processing performed. number of ships: ${
-      gameMap.myShips.length
-    }`
-  )
+  Log.log(`Number of planes found: ${gameMap.planets.length}`)
+    .log(`Number of ships found: ${gameMap.ships.length}`)
+    .log(`Number of friendly ships: ${gameMap.myShips.length}`)
+    .log(`Number of hostile ships: ${gameMap.enemyShips.length}`)
 }
 
 module.exports = preProcessor
